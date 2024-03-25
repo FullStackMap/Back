47b36bf3c1beee158e759cf812901f1abc9a7385
Dercraker <antoine.capitain@gmail.com>
2024-03-25 18:18:23 +0100

    Feat (Testimonial Dto) :  Add User to dto

--- Map.Domain/Models/Testimonial/TestimonialDto.cs
+++ Map.Domain/Models/Testimonial/TestimonialDto.cs
@@ -1,10 +1,13 @@
-﻿namespace Map.Domain.Models.Testimonial;
+﻿using Map.Domain.Models.Auth;
+
+namespace Map.Domain.Models.Testimonial;
 public class TestimonialDto
 {
     public int TestimonialId { get; set; }
     public string FeedBack { get; set; }
 
     public Guid UserId { get; set; }
+    public virtual MapUserDto? User { get; set; }
 
     public int rate { get; set; }
     public DateOnly TestimonialDate { get; set; }
